777f5aeb1ccb2c09f90c9f39975c3467fb04fb12
Amar Prakash Pandey <amar.om1994@gmail.com>
2021-06-05 01:16:14 +0530

    Change build tool from Maven to Gradle

---
+++ url-shortener-backend/src/test/java/me/amarpandey/UrlShortenerApplicationTests.java
@@ -0,0 +1,9 @@
+package me.amarpandey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+}
