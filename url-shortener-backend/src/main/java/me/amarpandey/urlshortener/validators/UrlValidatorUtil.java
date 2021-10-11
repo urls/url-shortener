431cd5cf40b87150ed6c1e8f96c98a55d39ea75b
ElzaCS <anuejohn@gmail.com>
2021-10-11 21:05:11 +0530

    Improve url-validation logic

---
+++ url-shortener-backend/src/main/java/me/amarpandey/urlshortener/validators/UrlValidatorUtil.java
@@ -0,0 +1,26 @@
+package me.amarpandey.urlshortener.validators;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+public class UrlValidatorUtil {
+
+    public static boolean isValidUrl(String url) {
+        try {
+            if (urlValidator(url)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+    public static boolean urlValidator(String url)    {
+        UrlValidator defaultValidator = new org.apache.commons.validator.routines.UrlValidator();
+        return defaultValidator.isValid(url);
+    }
+
+}
+
